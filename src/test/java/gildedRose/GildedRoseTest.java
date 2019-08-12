943cb291580bb0da51978d28f539021e1a376b19
zzwgo <kenzzw@163.com>
2019-08-12 20:20:06 +0800

    Add: test for ordinary item given sellIn 1,quality 0

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -15,4 +15,14 @@ public class GildedRoseTest {
         assertEquals("something, 0, 9", item.toString());
     }
 
+    @Test
+    public void should_return_something_0_0_given_sellIn_is_1_and_quality_is_0() {
+        Item item = new Item("something", 1, 0);
+        GildedRose gildedRose=new GildedRose(new Item[]{(item)});
+
+        gildedRose.updateQuality();
+
+        assertEquals("something, 0, 0", item.toString());
+    }
+
 }
