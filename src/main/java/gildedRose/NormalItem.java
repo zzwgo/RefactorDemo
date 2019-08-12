d420c38288db642ac78c0a3e1d2afb332f22b8bf
zzwgo <kenzzw@163.com>
2019-08-12 23:11:47 +0800

    Refactor: NormalItem method

--- src/main/java/gildedRose/NormalItem.java
+++ src/main/java/gildedRose/NormalItem.java
@@ -4,14 +4,15 @@ public class NormalItem implements ItemType {
 
     @Override
     public void update(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
+        handleQualityMoreThanZero(item);
         item.sellIn = item.sellIn - 1;
         if (item.sellIn < 0) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
+            handleQualityMoreThanZero(item);
+        }
+    }
+    private void handleQualityMoreThanZero(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
     }
 }
