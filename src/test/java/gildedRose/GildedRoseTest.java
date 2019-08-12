04a52af8e0834c838ed3e1569d9e8ebab6a686c0
zzwgo <kenzzw@163.com>
2019-08-12 20:12:08 +0800

    Add: test for GildedRose with ordinary item

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -5,6 +5,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class GildedRoseTest {
+    @Test
+    public void should_return_something_0_9_given_sellIn_is_1_and_quality_is_10() {
+        Item item = new Item("something", 1, 10);
+        GildedRose gildedRose=new GildedRose(new Item[]{(item)});
 
+        gildedRose.updateQuality();
+
+        assertEquals("something, 0, 9", item.toString());
+    }
 
 }
